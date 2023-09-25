0821d2f5f47412963d103281b2cf5bc34dfa6f2f
Lukas <lars.lukas.hellwig@gmail.com>
2023-09-25 23:01:00 +0200

    Portionierung

--- pages/1_🍴_recipe.py
+++ pages/1_🍴_recipe.py
@@ -3,32 +3,35 @@ import pandas as pd
 import json
 
 from pint import UnitRegistry
-from copy import deepcopy
 
 
 # Todos
 # - file read
 # - file write
 
+@st.cache_resource
+def get_unit_registry():
+    return UnitRegistry(fmt_locale='de_DE') # slow!
 
 class Zutat:
-    name: str
-    menge: UnitRegistry.Quantity
-    num: int
-    list: list
-    
-    def __init__(self, name: str, menge: str, unit_reg: UnitRegistry):
-        
-        self.name = name
-        self.menge = unit_reg(menge)
+    # class variables
+    einheiten = ["g", "kg", "l", "ml"]
 
-        self.list = []
-        self.list.append(self.name)
-        self.list.append(self.menge)
+    # instance variables
+    name: str
+    menge: int
+    einheit: str
+    i: int
+    
+    def __init__(self, name: str, menge: int, einheit: str):
+
+        self.name = name
+        self.menge = menge
+        self.einheit = einheit
 
 
     def __str__(self):
-        return str(self.menge) + " " + self.name
+        return str(self.menge) + " " + self.einheit + " " + self.name
     
     def __dict__(self): # for JSON dump
         # temp = {}
@@ -38,50 +41,60 @@ class Zutat:
         return
     
     def __iter__(self):
-        self.num  = 0
+        self.i  = 0
         return self
     
     def __next__(self):
-        if self.num <= 1:
-            self.num += 1
-        else:
-            raise StopIteration
-        
 
-        return self.list[self.num - 1]
+        self.i += 1
 
-    
+        match self.i:
+            case 1: return self.name
+            case 2: return str(self.menge) + " " + self.einheit
+            case 3: raise StopIteration
 
+ 
 class Rezept:
     name: str
     autor: str
+
     Zutaten: dict
 
     portionen: int # die Standard-Portionszahl
+    Zutaten_portioniert: dict
 
-    unit_reg: UnitRegistry
-
-
-    def __init__(self, name: str, autor: str,  zutaten: dict, portionen: int, unit_registry: UnitRegistry): # warum wird dict angenommen?
+    def __init__(self, name: str, autor: str,  zutaten: dict, portionen: int):
         self.name = name
         self.autor = autor
         self.Zutaten  = zutaten
         self.portionen = portionen
-        self.unit_reg = unit_registry
+
+        self.Zutaten_portioniert = {}
+
+        for key in self.Zutaten:
+            z = self.Zutaten[key]
+            self.Zutaten_portioniert[key] = Zutat(z.name, z.menge, z.einheit)
+
+
 
     def add_zubereitung(self, beschreibung: str):
         self.beschreibung = beschreibung # Zutaten werden rein formatiert
+
+    def portionieren(self, portionen: int):
+        for key in self.Zutaten_portioniert:
+            self.Zutaten_portioniert[key].menge *= portionen
+        return
     
-    def get_zubereitung(self, portionen = 0):
-        if portionen == 0: 
-            portionen = self.portionen
-            #Todo: Zutaten * menge
 
-        temp_Zutaten = {}
-        for Zutat in self.Zutaten:
-            temp_Zutaten[Zutat.name] = str(Zutat.menge), self.unit_reg
+        # self.Zutaten_portioniert = copy.deepcopy(self.Zutaten)
+        # print("Zutaten")
+        # print(self.Zutaten)
+        # print("portioniert")
+        # print(self.Zutaten_portioniert)
 
-        return self.beschreibung.format(**temp_Zutaten)
+
+    def get_zubereitung(self):
+        return self.beschreibung.format(**self.Zutaten_portioniert)
 
 
 class Kochbuch:
@@ -95,35 +108,19 @@ class Kochbuch:
         self.Rezepte[rezept.name] = rezept
 
 
-
 st.sidebar.markdown("# Rezepte 🍴")
 st.write("## Rezepte 🍴")
 
 
-# --------------- Units -------------------------------
-unit_reg = UnitRegistry()
-Q = unit_reg.Quantity
-Q("200g").to("kg")
-
-st.write('Variable =  {:P}'.format(Q("10 m/s^2"))) # Markdown
-st.latex('Variable =  {:L}'.format(Q("10 m/s^2"))) # LaTex
-st.write('Variable =  {:H}'.format(Q("10 m/s^2"))) # html
-
-# zutat1 = Zutat("Salz", 100, "gram", unit_reg)
-# print(zutat1.menge.to("kg"))
-# zutat1.menge
-
-# st.balloons()
-# st.toast('Your Rezipe was saved!', icon='😍')
-
-unit_reg = UnitRegistry(fmt_locale='de_DE')
-
+st.button("Rezept erstellen")
+st.button("Rezept bearbeiten")
 
 
 rezept = Rezept( "Schinkenbrot", "Lukas", {
-    "Zutat 1": Zutat("Schinken", "100 g", unit_reg),
-    "Zutat 2": Zutat("Brot", "100 g", unit_reg)
-}, 4, unit_reg)
+    "Zutat 1": Zutat("Schinken", 200, "g"),
+    "Zutat 2": Zutat("Brot", 100, "g")
+}, 4)
+
 
 
 rezept.add_zubereitung('''Zubereitung:
@@ -133,13 +130,14 @@ rezept.add_zubereitung('''Zubereitung:
 ''') # {Zutat 1} kann von str.format() nachgeschlagen werden.
 
 rezept2 = Rezept( "Käsebrot", "Lukas", {
-    "Zutat 1": [100, "g", "Käse"]
-}, 4, unit_reg)
+    "Zutat 1": Zutat("Käse", 200, "g"),
+    "Zutat 2": Zutat("Brot", 100, "g")
+}, 4)
 
 rezept2.add_zubereitung('''Zubereitung:
 1. Schritt: Wasser zum kochen bringen
 2. Schritt: Zwiebeln schälen und schneiden
-3. Schritt: {Zutat 1[0]}
+3. Schritt: {Zutat 1}
 ''')
 
 dieses_kochbuch = Kochbuch("Lukas' Kochbuch")
@@ -156,29 +154,32 @@ radio_select = st.radio("Rezepte", dieses_kochbuch.Rezepte.keys())
 sel_recipe = dieses_kochbuch.Rezepte[radio_select]
 
 
+col1 , col2, col3, col4 = st.columns([1,1,1,2])
+
+with col1:
+    "Portionen"
+
+with col2:
+    portionen = int(st.text_input("", value = "4", label_visibility='collapsed'))
+
+with col3:
+    st.button("Zurücksetzen")
+    
+
 sel_recipe.name
 sel_recipe.autor
 
-st.table(pd.DataFrame(sel_recipe.Zutaten, index = ["Zutat", "Menge"]).transpose())
+sel_recipe.portionieren(portionen)
 
-
-portionen = 4
-st.write(sel_recipe.get_zubereitung(portionen))
-
-
+st.table(pd.DataFrame(sel_recipe.Zutaten_portioniert, index = ["Zutat", "Menge"]).transpose())
+st.write(sel_recipe.get_zubereitung())
 
 
 
+unit_reg = get_unit_registry()
 
-    # st.write(dieses_kochbuch.Rezepte[key])
-
-# st.text_input("Portionen", key="n")
-# Zutaten_output = pd.DataFrame(rezept.Zutaten, index = ["Menge", "Einheit", "Zutat"]).transpose()
-
-
-# st.dataframe(Zutaten_output)  
 
 
 # ---------------------- DEBUG -------------------------------------------------------------
-st.write("## Debug")
-st.json(json.dumps(dieses_kochbuch, default=lambda o: o.__dict__, sort_keys=False, indent=4))
+# st.write("## Debug")
+# st.json(json.dumps(dieses_kochbuch, default=lambda o: o.__dict__, sort_keys=False, indent=4))
